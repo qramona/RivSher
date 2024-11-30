7672f0526a9981ea603066409305494536b07a4f
Nataliya Bannova <nataliabannova.techspec@gmail.com>
2024-11-30 11:46:40 +0300

    initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import style from "./style.module.css";
+
+const Header = () => {
+  return (
+    <header className={style.header}>
+      <div className="container"></div>
+      <h1>Header</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <section>
+      <div className="container">
+        <h1>Закажи речное такси и&nbsp;не&nbsp;теряй время в&nbsp;пробках</h1>
+      </div>
+      <div className="map"></div>
+    </section>
+  );
+};
+
+export default Hero;
